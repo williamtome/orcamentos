774b5a0d175b865efa178231c34f4312b7cd99d9
William Tome <william.tomenh@gmail.com>
2022-12-29 19:10:26 -0300

    feat(lista-orcamentos): create a command to show the budgets.

---
+++ lista-orcamentos.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use William\DesignPattern\Orcamento;
+
+$orcamento1 = new Orcamento();
+$orcamento1->setValor(1230);
+$orcamento1->quantidadeDeItens = 7;
+$orcamento1->aprova();
+
+$orcamento2 = new Orcamento();
+$orcamento2->setValor(250);
+$orcamento2->quantidadeDeItens = 2;
+$orcamento2->reprova();
+
+$orcamento3 = new Orcamento();
+$orcamento3->setValor(2540);
+$orcamento3->quantidadeDeItens = 5;
+$orcamento3->aprova();
+$orcamento3->finaliza();
+
+$listaOrcamentos = [
+    $orcamento1,
+    $orcamento2,
+    $orcamento3,
+];
+
+foreach ($listaOrcamentos as $orcamento) {
+    echo 'Valor: R$ ' . $orcamento->getValor() . PHP_EOL;
+    echo 'Estado: ' . get_class($orcamento->estadoAtual) . PHP_EOL;
+    echo 'Itens: ' . $orcamento->quantidadeDeItens . PHP_EOL;
+
+    echo PHP_EOL;
+}
