dfb28089ce1780151568454f4e928aae61916cce
William Tome <william.tomenh@gmail.com>
2023-01-10 09:02:22 -0300

    fix(Imposto): call getValor method.

--- src/Impostos/Csll.php
+++ src/Impostos/Csll.php
@@ -8,16 +8,16 @@ class Csll extends ImpostoCom2Aliquotas
 {
     protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
     {
-        return $orcamento->valor > 4000;
+        return $orcamento->getValor() > 4000;
     }
 
     protected function calculaTaxaMaxima(Orcamento $orcamento): float
     {
-        return $orcamento->valor * 0.15;
+        return $orcamento->getValor() * 0.15;
     }
 
     public function calculaTaxaMinima(Orcamento $orcamento): float
     {
-        return $orcamento->valor * 0.09;
+        return $orcamento->getValor() * 0.09;
     }
 }
